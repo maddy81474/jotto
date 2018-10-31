29aa866c14e81672202c2323b5d5045c1fa04df7
unknown <Madhu.Surendra@NEVDES065.NEVITON.IN>
2018-10-31 14:16:00 +0530

    Add protoTypes for Congrats component

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -3,15 +3,17 @@ import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import Congrats from './Congrats';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
 Enzyme.configure({adapter : new EnzymeAdapter()});
 
+const defaultProps = {success: false};
 const setup = (props={}) => {
-    return shallow(<Congrats {...props} />);
+    const setupProps = {...defaultProps, ...props}
+    return shallow(<Congrats {...setupProps} />);
 };
 
-test('render withour error', () => {
+test('render without error', () => {
     const wrapper = setup();
     const component = findByTestAttr(wrapper, 'component-congrats');
     expect(component.length).toBe(1);
@@ -28,4 +30,9 @@ test('renders non empty congrats message when `success` prop is true' , () => {
     const wrapper = setup({success: true});
     const message = findByTestAttr(wrapper, 'congrats-message');
     expect(message.text().length).not.toBe(0);
+});
+
+test('does not throw warning with expected props', () => {
+    const expectedProps = { success: false };
+    checkProps(Congrats, expectedProps);
 });
