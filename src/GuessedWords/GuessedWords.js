1e14ece67a9ecc845a9bb27ff4d92391111cb287
unknown <Madhu.Surendra@NEVDES065.NEVITON.IN>
2018-11-02 13:35:37 +0530

    Tests and code for non-zero guessed words

--- src/GuessedWords/GuessedWords.js
+++ src/GuessedWords/GuessedWords.js
@@ -7,6 +7,31 @@ const GuessedWords = (props) => {
         contents = (
             <span data-test='guess-instructions'>Try To guess the secret word</span>
         )
+    } else {
+        let guessedWordsRows = props.guessedWords.map((guessedWord , index) => {
+            return (
+                <tr data-test="guessed-word" key={index}>
+                    <td>{guessedWord.guessedWord}</td>
+                    <td>{guessedWord.letterMatchCount}</td>
+                </tr>
+            )
+        });
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching Letters</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { guessedWordsRows }
+                    </tbody>
+                </table>
+            </div>
+        );
     }
     return (
         <div data-test="component-guessed-words">
