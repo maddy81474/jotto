19f1fbf326f564a42581d7341c94981202d8e5ab
unknown <Madhu.Surendra@NEVDES065.NEVITON.IN>
2018-11-08 18:08:06 +0530

    input test and component

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -3,16 +3,26 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
     render(){
+        const contents = this.props.success ? null : (
+            <form className="form-inline">
+                <input data-test="input-box" 
+                className="mb-2 mx-sm-3"
+                id="word-guess"
+                type="text"
+                placeholder="enter the guess" />
+                <button data-test="submit-button" type="submit" className="btn btn-primary mb-2">Submit</button>
+            </form>
+        );
         return (
-            <div>
-                <button />
+            <div data-test="component-input">
+                { contents }
             </div>
         )
     }
 };
 
 const mapStatesToProps = (state) => {
-    return {};
+    return state;
 }
 
 export default connect(mapStatesToProps)(Input);
--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,10 +1,16 @@
 import { actionTypes } from '../actions';
 
-export default (state=false,action) => {
-    switch(action.type){
-        case (actionTypes.CORRECT_GUESS): 
-        return true;
-    default: 
-        return state;
-    }
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+export default (state=false, action) => {
+  switch(action.type) {
+    case (actionTypes.CORRECT_GUESS):
+      return true;
+    default:
+      return state;
+  }
 }
