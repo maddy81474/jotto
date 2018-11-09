fe535f8c3da5cd085082581f17599ed614846037
unknown <Madhu.Surendra@NEVDES065.NEVITON.IN>
2018-11-09 11:56:32 +0530

    guessedWords acton created

---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe("guessWord action dispatcher", () => {
+    describe("no guess words ", () => {
+        test('updates state correctly for unsuccesfull guess', () => {
+            
+        }); 
+        test('updates state correctly for unsuccesfull guess', () => {
+
+        }); 
+    })
+    describe("some  guess words ", () => {
+        test('updates state correctly for unsuccesfull guess', () => {
+            
+        }); 
+        test('updates state correctly for unsuccesfull guess', () => {
+
+        }); 
+    })
+});
